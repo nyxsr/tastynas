1c49e12736584791dbbe6096ff9335a49c11cab6
nyxsraid <sahrulramdan.75@gmail.com>
2022-09-15 00:26:37 +0700

    Section Testimoni & Spotlight & Footer has been Added and Optimizing Responsiveness

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,74 @@
+<footer class="footer">
+    <div class="container">
+        <div class="footer-middle">
+            <div class="row" style="width: 100vw; justify-content: center">
+                <div class="col-lg-4 col-sm-6 mb-6 mb-lg-0">
+                    <div class="widget widget-about">
+                        <a href="demo8.html" class="logo-footer mb-5">
+                            <img src="{{ asset('assets/images/first-logo.png') }}" alt="logo-footer" width="171"
+                                height="171" />
+                                <div class="wrapper-social">
+                                    <a href="#" title="Facebook" class="social-link fab fa-facebook-f"></a>
+                                    <a href="#" title="Instagram" class="social-link fa-brands fa-instagram"></a>
+                                    <a href="#" title="TikTok" class="social-link fa-brands fa-tiktok"></a>
+                                    <a href="#" title="YouTube" class="social-link fa-brands fa-youtube mr-0"></a>
+                                </div>
+                        </a>
+                    </div>
+                    <!-- End of Widget -->
+                </div>
+                <div class="col-lg-8col1 col-sm-6 mb-6 mb-lg-0">
+                    <div class="widget">
+                        <h4 class="widget-title">Our Story</h4>
+                        <ul class="widget-body">
+                            <li><a href="about.html">About us</a></li>
+                            <li><a href="#">Sertifikasi</a></li>
+                            <li><a href="#">Best Seller</a></li>
+                            <li><a href="#">Keunggulan</a></li>
+                        </ul>
+                    </div>
+                    <!-- End of Widget -->
+                </div>
+                <div class="col-lg-8col1 col-sm-6 mb-6 mb-lg-0">
+                    <div class="widget">
+                        <h4 class="widget-title">Contact info</h4>
+                        <ul class="widget-body footer-icon-boxes">
+                            <li>
+                                <a href="tel:#" class="footer-icon-box">
+                                    <i class="p-icon-phone-solid"></i>
+                                    <span>+6281 3940 64004</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="">
+                                    <i class="p-icon-map"></i>
+                                    <span>Jalan Kh Agus Salim &, Gg. Jenaka, Cigadung, Kec. Subang, Kabupaten Subang, Jawa Barat 41211</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="mailto:mail@panda.com" class="">
+                                    <i class="p-icon-message"></i>
+                                    <span style="text-transform: none !important">delipel@gmail.com</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="">
+                                    <i class="p-icon-clock"></i>
+                                    <span>Buka 07.00-17.00 (Senin - Minggu)</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- End of Widget -->
+                </div>
+            </div>
+        </div>
+        <!-- End FooterMiddle -->
+        <div class="footer-bottom">
+            <div class="footer-left">
+                <p class="copyright">Delipel - Tastynas © 2022. All Rights Reserved</p>
+            </div>
+        </div>
+        <!-- End FooterBottom -->
+    </div>
+</footer>
